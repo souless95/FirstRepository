7844571a81dee2de03c936d5091f4698d81d92be
souless95 <yj6807@gmail.com>
2022-11-07 14:39:36 +0900

    두번째 작업 완료

--- src/git/HelloGit.java
+++ src/git/HelloGit.java
@@ -5,7 +5,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 		
 		System.out.println("git연동해보기");
-
+		System.out.println("두번째 작업 끝");
 	}
 
 }
--- src/git/NewFile.java
+++ src/git/NewFile.java
@@ -5,7 +5,7 @@ public class NewFile {
 	public static void main(String[] args) {
 
 		System.out.println("두번째파일");
-
+		System.out.println("이것두 두번째 작업 끝");
 	}
 
 }
